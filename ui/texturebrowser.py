abadae756d5517a271683bba90803d67fccaa29d
jmickle66666666 <jaymickle@gmail.com>
2017-03-22 12:25:46 +0000

    More TextureBrowser work
    ui/texturebrowser.py, switched to a canvas element to render the
    available textures. texture previews are available and dynamically
    positioned
    ui/resutil.py, added a function to get an image and automatically
    resize it to specified dimensions

--- ui/texturebrowser.py
+++ ui/texturebrowser.py
@@ -1,18 +1,33 @@
 # The UI and functionality for browsing and selecting textures from texture collections
-from Tkinter import Tk
-from ttk import *
-import res
+from Tkinter import *
 from util import resutil
+import res
 
 
 # A single texture element. A square containing the preview and name of the texture
-class TextureItem(Frame):
-    def __init__(self, parent, name, photoimage):
-        Frame.__init__(self, parent)
+class TextureItem:
+    def __init__(self, name, photoimage, canvas):
+        self.x = 0
+        self.y = 0
+        self.name = name
+        self.image = photoimage
+        self.canvas = canvas
 
-        self.image_label = Label(self, text=name, image=photoimage, width=20, compound="top")
-        self.image_label.photo = photoimage
-        self.image_label.pack(expand=0)
+        text_offset_x = 5
+        text_offset_y = 3
+        text_bg_color = "#999999"
+
+        self.image_id = self.canvas.create_image((self.x, self.y), image=self.image, anchor="nw")
+        self.text_id = self.canvas.create_text((self.x + text_offset_x, self.y + text_offset_y), text=name, anchor="nw")
+        self.rect_id = self.canvas.create_rectangle(self.canvas.bbox(self.text_id), fill=text_bg_color)
+        self.canvas.tag_raise(self.text_id)
+
+    def move_to(self, x, y):
+        self.canvas.move(self.image_id, x - self.x, y - self.y)
+        self.canvas.move(self.text_id, x - self.x, y - self.y)
+        self.canvas.move(self.rect_id, x - self.x, y - self.y)
+        self.x = x
+        self.y = y
 
 
 # The panel to list all the textures in a single collection
@@ -21,9 +36,34 @@ class TextureGrid(Frame):
         Frame.__init__(self, parent)
         self.config(width=200, height=200)
 
-        self.splashimage = resutil.load_photoimage(res.temp_splash)
-        self.test_item = TextureItem(self, "testname", self.splashimage)
-        self.test_item.pack()
+        self.canvas = Canvas(self, width=500, height=500)
+        self.canvas.pack()
+
+        self.texture_items = []
+
+        # this value is how many items it can currently fit in a row.
+        self.row_item_count = 2
+
+        # this is how much space is between items
+        self.item_space = 110
+
+        self.photo = resutil.load_photoimage_resized(res.temp_splash, (100, 100))
+        self.add_item("test1", self.photo)
+        self.add_item("test2", self.photo)
+        self.add_item("test3", self.photo)
+        self.reposition_items()
+
+
+    # Add a new texture item
+    def add_item(self, name, image):
+        self.texture_items.append(TextureItem(name, image, self.canvas))
+
+    # Reposition all the texture items to the current row count
+    def reposition_items(self):
+        i = 0
+        for t in self.texture_items:
+            t.move_to((i % self.row_item_count) * self.item_space, (i // self.row_item_count) * self.item_space)
+            i += 1
 
 
 # A list to show and select one of the currently available texture collections to browse
@@ -31,6 +71,9 @@ class TextureCollectionList(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.config(width=200)
+
+        self.junk_text = Label(self, text="TextureCollection 1\nTextureCollection 2\netc")
+        self.junk_text.pack()
 
 
 # The master window for the texture browser
--- util/resutil.py
+++ util/resutil.py
@@ -11,3 +11,9 @@ def load_image(data):
 # Returns a Tkinter-compatible PhotoImage from a base64 data
 def load_photoimage(data):
     return ImageTk.PhotoImage(load_image(data))
+
+
+# Returns a Tkinter-compatible PhotoImage at the specified size
+def load_photoimage_resized(data, size):
+    image = load_image(data).resize(size)
+    return ImageTk.PhotoImage(image)
