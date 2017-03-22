039088fa5b98997f64ceb76477200c45bbe3909e
jmickle66666666 <jaymickle@gmail.com>
2017-03-22 14:38:13 +0000

    More texturebrowser work
    ui/texturebrowser.py, some styling, added scrollbar, added scrolling,
    added rowsize calculation, added texturecollection loading

--- ui/texturebrowser.py
+++ ui/texturebrowser.py
@@ -1,7 +1,7 @@
 # The UI and functionality for browsing and selecting textures from texture collections
 from Tkinter import *
-from util import resutil
-import res
+from util import texturecollection
+from PIL import ImageTk
 
 
 # A single texture element. A square containing the preview and name of the texture
@@ -17,9 +17,17 @@ class TextureItem:
         text_offset_y = 3
         text_bg_color = "#999999"
 
-        self.image_id = self.canvas.create_image((self.x, self.y), image=self.image, anchor="nw")
-        self.text_id = self.canvas.create_text((self.x + text_offset_x, self.y + text_offset_y), text=name, anchor="nw")
-        self.rect_id = self.canvas.create_rectangle(self.canvas.bbox(self.text_id), fill=text_bg_color)
+        self.image_id = self.canvas.create_image((self.x, self.y),
+                                                 image=self.image,
+                                                 anchor="nw")
+
+        self.text_id = self.canvas.create_text((self.x + text_offset_x, self.y + text_offset_y),
+                                               text=name,
+                                               anchor="nw")
+
+        self.rect_id = self.canvas.create_rectangle(self.canvas.bbox(self.text_id),
+                                                    fill=text_bg_color,
+                                                    outline=text_bg_color)
         self.canvas.tag_raise(self.text_id)
 
     def move_to(self, x, y):
@@ -37,22 +45,26 @@ class TextureGrid(Frame):
         self.config(width=200, height=200)
 
         self.canvas = Canvas(self, width=500, height=500)
-        self.canvas.pack()
+        self.canvas.config(bg="#252321")
+        self.canvas.pack(side=LEFT)
+
+        self.scrollbar = Scrollbar(self, orient=VERTICAL)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.scrollbar.config(command=self.canvas.yview)
+
+        self.canvas.config(yscrollcommand=self.scrollbar.set)
 
         self.texture_items = []
-
-        # this value is how many items it can currently fit in a row.
-        self.row_item_count = 2
+        self.row_item_count = 1
 
         # this is how much space is between items
         self.item_space = 110
 
-        self.photo = resutil.load_photoimage_resized(res.temp_splash, (100, 100))
-        self.add_item("test1", self.photo)
-        self.add_item("test2", self.photo)
-        self.add_item("test3", self.photo)
-        self.reposition_items()
-
+        # self.photo = resutil.load_photoimage_resized(res.temp_splash, (100, 100))
+        # self.add_item("test1", self.photo)
+        # self.add_item("test2", self.photo)
+        # self.add_item("test3", self.photo)
+        # self.reposition_items()
 
     # Add a new texture item
     def add_item(self, name, image):
@@ -60,10 +72,26 @@ class TextureGrid(Frame):
 
     # Reposition all the texture items to the current row count
     def reposition_items(self):
+        self.calculate_row_size()
         i = 0
         for t in self.texture_items:
-            t.move_to((i % self.row_item_count) * self.item_space, (i // self.row_item_count) * self.item_space)
+            t.move_to((i % self.row_item_count) * self.item_space + 10,
+                      (i // self.row_item_count) * self.item_space + 10)
             i += 1
+
+        itemcount = (len(self.texture_items) // self.row_item_count) + 1
+        self.canvas.config(scrollregion=(0, 0, 0, 20 + (itemcount * self.item_space)))
+
+    def load_texture_collection(self, tc):
+        self.texture_items = []
+        for t in tc.textures:
+            thumbnail = tc.textures[t].resize((100, 100))
+            photoimage = ImageTk.PhotoImage(thumbnail)
+            self.add_item(t, photoimage)
+        self.reposition_items()
+
+    def calculate_row_size(self):
+        self.row_item_count = max((self.canvas.winfo_reqwidth()+20) // 110, 1)
 
 
 # A list to show and select one of the currently available texture collections to browse
@@ -94,5 +122,8 @@ class TextureBrowser(Tk):
 
 
 if __name__ == "__main__":
+    test_textures_path = "/Users/jerry.micklethwaite/Dropbox/projects/games/2016/SLDR/metals/"
+    tc = texturecollection.TextureCollection.load_folder(test_textures_path)
     app = TextureBrowser(None)
+    app.texture_grid.load_texture_collection(tc)
     app.mainloop()
